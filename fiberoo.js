992b21f33e281e86a4a782bf73608a72be4ad8aa
Joshua Cruz <cruzj4@hawaii.edu>
2019-02-06 22:33:44 -1000

    Coding Standards: Inclass Practice WOD

---
+++ fiberoo.js
@@ -0,0 +1,19 @@
+function fib() {
+  const fibList = [0, 1];
+  for (let i = 2; i < 100; i++) {
+    fibList.push(fibList[i - 1] + fibList[i - 2]);
+  }
+  return fibList;
+}
+
+console.log(fib());
+
+function numsToStrings(nums) {
+  return _.map(nums, num => ${num} );
+}
+console.log(numsToStrings(fib()));
+
+function numEvenNums(nums) {
+  return _.filter(nums, num => ((num % 2) === 0)).length;
+}
+console.log(numEvenNums(fib()));
